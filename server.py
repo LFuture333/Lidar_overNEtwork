f7efacc0f23d87ac0da372a2077e2378069271e1
Certified Humane <castrolwilliam@gmail.com>
2023-10-25 13:33:33 -0400

    Starte Writing
    structuring the class for server

--- server.py
+++ server.py
@@ -4,10 +4,25 @@ import sys
 from collections import namedtuple
 
 class Server:
+# Declaring the socket for the server
+    server = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP)
+    server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEPORT, 1)
+    server.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
+
+    server.settimeout(0.2)
+
+# Declare the lidar parameters 
+
+
     Lidar_Data = namedtuple('Lidar', ['x','y', 'Loop_Count'])
 
     def __init__(self):
         print()
-        struct = self.Lidar(x=[4.0,4,4,3], y=[4.0,4,4,3], Loop_Count=0)
 
-        print(struct)
+
+
+
+    
+
+
+    
