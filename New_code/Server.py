4abe24788ea675924a049caac9f0d30577a29db2
Certified Humane <castrolwilliam@gmail.com>
2023-11-01 00:13:26 -0400

    ii

--- New_code/Server.py
+++ New_code/Server.py
@@ -1,1 +1,48 @@
+from socket import *
+import time 
+import sys
+import Lidar
+import matplotlib.pyplot as plt
+import base64
 
+
+class Server:
+
+    def initate_socket(self):
+        ip = input("Enter Ip address: ")
+
+
+        self.address = (ip, 100)
+        self.s = socket(type=SOCK_DGRAM)
+
+        self.s.settimeout(0.2)
+        self.s.bind(self.address)
+
+    def Decrypting_Data(self, Data):
+        decode_data = base64.b64decode(Data)
+
+        point = decode_data.decode('utf-8')
+
+        return point
+    
+
+
+    def Recv_Data(self):
+        try:    
+            while True:
+                x= 0 
+                y = 0
+                data, addr = self.s.recvfrom(1024)
+
+                point = self.Decrypting_Data(data)
+
+                plt.clf()
+                plt.scatter(x, y)
+                plt.pause(0.1)
+
+            plt.show()
+
+
+        except KeyboardInterrupt:
+            self.s.close()
+            sys.exit()
